74b4156024028aa2983abd5942c3b936df85ba88
Kovács Ádám <kovacsadam@MacBook-Pro.localdomain>
2022-10-27 18:05:15 +0200

    main menu

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+define('SERVER_ROOT', $_SERVER['DOCUMENT_ROOT'].'kovacsadam/WEBII-B01/');
+define('SITE_ROOT', 'http://localhost/kovacsadam/WEBII-B01/');
+require_once(SERVER_ROOT . 'controllers/' . 'router.php');
+?>
---
+++ views/nyitolap_main.php
@@ -0,0 +1,23 @@
+<body class="nyit_background"></body>
+ <div class="kulso">
+ <div class="belso">
+	    
+				<img style=" max-width: 100%; max-height: 100%;" src="./images/kep1.jpg" class="kep1">
+				
+				<div class="cim">
+				<h1><b>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat, maxime!</b></h1></div>
+				<div class="szoveg">
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias ea totam magnam odio, quas quam voluptates dolore? Consequuntur, magnam est nulla voluptates dolore ex repudiandae ad reprehenderit, laudantium neque corrupti.
+</div>
+<div class="egyes">
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi eum sint natus, voluptatum numquam architecto labore esse necessitatibus? Fuga, ipsam.
+	</div>
+	<div class="kettes">
+				<div class="cim">
+				<h1><b>Lorem, ipsum dolor.</b></h1></div>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quis nam fugiat rerum nesciunt, temporibus, harum porro minus tempore accusamus dignissimos quo iusto tempora amet odio sequi ab sapiente ad.
+ </div>
+<div class="szoveg">
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora eveniet, nobis, nulla similique ipsum ducimus fugit id molestiae nisi harum iste! Suscipit quos esse sit quaerat asperiores vero, placeat temporibus, ullam est ipsum, architecto perspiciatis consequatur ut eligendi sapiente debitis.
+</div> 				
+</div>
