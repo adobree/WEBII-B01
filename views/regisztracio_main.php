7b6fa95075ada7ec0097b7bfbeb0c4d4d64f8b29
Adam Kovacs <ado.kovacs@gmail.comm>
2022-11-10 08:50:54 +0100

    refactor regisztracio_main.php

--- views/regisztracio_main.php
+++ views/regisztracio_main.php
@@ -7,15 +7,15 @@
 <legend>Regisztráció</legend>
     <label class='reg_label' for="csaladi_nev">Családi név:</label>
     <input class='reg_input' type="text" name="csaladi_nev" id="csaladi_nev"
-    required pattern="[a-zA-Z][\-\.a-zA-Z0-9_]{3}[\-\.a-zA-Z0-9_]+"><br>
+    required><br>
 	<label class='reg_label' for="utonev">Utónév:</label>
     <input class='reg_input' type="text" name="utonev" id="utonev"
-	required pattern="[a-zA-Z][\-\.a-zA-Z0-9_]{3}[\-\.a-zA-Z0-9_]+"><br>
+	required><br>
     <label class='reg_label' for="bejelenkezes">Felhasználónév:</label>
-    <input class='reg_input' type="text" name="bejelentkezes" id="bejelentkezes"><br>
+    <input class='reg_input' type="text" name="bejelentkezes" id="bejelentkezes" required><br>
     <label class='reg_label' for="jelszo">Jelszó:</label>
     <input class='reg_input' type="password" class="re" name="jelszo" id="jelszo" 
-	required pattern="[\-\.a-zA-Z0-9_]{4}[\-\.a-zA-Z0-9_]+"><br>
+	required minlength="5"><br>
     <input class='btn_send' type="submit" value="Küldés">
 </fieldset>
 </form>
